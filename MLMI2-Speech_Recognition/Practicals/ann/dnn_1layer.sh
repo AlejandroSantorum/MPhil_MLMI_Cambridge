efec0aa545aa613bf8fcb7d92a0e7167e01da4dd
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-12-07 13:29:29 +0000

    SpeechRec - add script + results + img of DNN-1Layer

--- MLMI2-Speech_Recognition/Practicals/ann/dnn_1layer.sh
+++ MLMI2-Speech_Recognition/Practicals/ann/dnn_1layer.sh
@@ -1,8 +1,6 @@
 
-fname_train_res=./results/1layer_train_res.txt
-fname_cv_res=./results/1layer_cv_res.txt
-fname_test_res=./results/1layer_test_res.txt
-
+fname_res=./results/dnn_1layer.txt
+fname_plot=./imgs/dnn_1layer.png
 
 limInf=1
 jump=2
@@ -17,7 +15,7 @@ fi
 
 if [ "$1" == "-d" ]; then # Deliting previous execution folders if specified
     rm -rf `find ./${store_folder} -name '*MH1*'`
-    rm -rf $fname_train_res $fname_cv_res $fname_test_res
+    rm -rf $fname_res
 fi
 
 ######### MFCCs #########
@@ -39,17 +37,17 @@ do
         # Training
         ../tools/steps/step-dnntrain -MODELINI ../new_nn_models/dnn/DNN-1L.ReLU.MFCC${param}.ini -GPUID 0 -vvv ../convert/mfc13d/env/environment${param} $pracgmmexp_mfc/align-mono-hmm84/align/timit_train.mlf $pracgmmexp_mfc/mono/hmm84/MMF $pracgmmexp_mfc/mono/hmms.mlist ${store_folder}/MH1-MFC${param}-CW${context_width}/dnntrain
         # Fetching training accuracies: CV set accuracy and train accuracy
-        echo "===== ANN1Layer_MFC${param}_FlatStart contextWidth=${context_width} =====" >> $fname_train_res
+        echo "===== DNN1L_MFC${param}_FlatStart Train contextWidth=${context_width} =====" >> $fname_res
         trainAcc=$(grep 'Train Accuracy' ${store_folder}/MH1-MFC${param}-CW${context_width}/dnntrain/hmm0/LOG | tail -1)
-        echo $trainAcc >> $fname_train_res
-        echo "===== ANN1Layer_MFC${param}_FlatStart contextWidth=${context_width} =====" >> $fname_cv_res
+        echo $trainAcc >> $fname_res
+        echo "===== DNN1L_MFC${param}_FlatStart CV contextWidth=${context_width} =====" >> $fname_res
         cvAcc=$(grep 'Validation Accuracy' ${store_folder}/MH1-MFC${param}-CW${context_width}/dnntrain/hmm0/LOG | tail -1)
-        echo $cvAcc >> $fname_cv_res
+        echo $cvAcc >> $fname_res
         # Decoding, i.e., Testing
         ../tools/steps/step-decode -INSWORD -8 -BEAMWIDTH 200 $PWD/${store_folder}/MH1-MFC${param}-CW${context_width}/dnntrain hmm0 ${store_folder}/MH1-MFC${param}-CW${context_width}/decode
-        echo "===== ANN1Layer_MFC${param}_FlatStart contextWidth=${context_width} =====" >> $fname_test_res
+        echo "===== DNN1L_MFC${param}_FlatStart Test contextWidth=${context_width} =====" >> $fname_res
         testAcc=$(grep 'WORD:' ${store_folder}/MH1-MFC${param}-CW${context_width}/decode/test/LOG | tail -1)
-        echo $testAcc >> $fname_test_res
+        echo $testAcc >> $fname_res
     done
 done
 
@@ -72,16 +70,21 @@ do
         # Training
         ../tools/steps/step-dnntrain -MODELINI ../new_nn_models/dnn/DNN-1L.ReLU.FBANK${param}.ini -GPUID 0 -vvv ../convert/fbk25d/env/environment${param} $pracgmmexp_fbk/align-mono-hmm84/align/timit_train.mlf $pracgmmexp_fbk/mono/hmm84/MMF $pracgmmexp_fbk/mono/hmms.mlist ${store_folder}/MH1-FBK${param}-CW${context_width}/dnntrain
         # Fetching training accuracies: CV set accuracy and train accuracy
-        echo "===== ANN1Layer_FBK${param}_FlatStart contextWidth=${context_width} =====" >> $fname_train_res
+        echo "===== DNN1L_FBK${param}_FlatStart Train contextWidth=${context_width} =====" >> $fname_res
         trainAcc=$(grep 'Train Accuracy' ${store_folder}/MH1-FBK${param}-CW${context_width}/dnntrain/hmm0/LOG | tail -1)
-        echo $trainAcc >> $fname_train_res
-        echo "===== ANN1Layer_FBK${param}_FlatStart contextWidth=${context_width} =====" >> $fname_cv_res
+        echo $trainAcc >> $fname_res
+        echo "===== DNN1L_FBK${param}_FlatStart CV contextWidth=${context_width} =====" >> $fname_res
         cvAcc=$(grep 'Validation Accuracy' ${store_folder}/MH1-FBK${param}-CW${context_width}/dnntrain/hmm0/LOG | tail -1)
-        echo $cvAcc >> $fname_cv_res
+        echo $cvAcc >> $fname_res
         # Decoding, i.e., Testing
         ../tools/steps/step-decode -INSWORD -8 -BEAMWIDTH 200 $PWD/${store_folder}/MH1-FBK${param}-CW${context_width}/dnntrain hmm0 ${store_folder}/MH1-FBK${param}-CW${context_width}/decode
-        echo "===== ANN1Layer_FBK${param}_FlatStart contextWidth=${context_width} =====" >> $fname_test_res
+        echo "===== DNN1L_FBK${param}_FlatStart Test contextWidth=${context_width} =====" >> $fname_res
         testAcc=$(grep 'WORD:' ${store_folder}/MH1-FBK${param}-CW${context_width}/decode/test/LOG | tail -1)
-        echo $testAcc >> $fname_test_res
+        echo $testAcc >> $fname_res
     done
 done
+
+# Plotting results
+echo "Plotting results"
+xaxis="Context window width"
+python3 ./mult_models_TCVT_plot.py $fname_res $fname_plot $xaxis
