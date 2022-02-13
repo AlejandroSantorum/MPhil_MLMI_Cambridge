99f8864be264c327a29554cbe5657a51341795be
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-02-13 17:00:39 +0000

    MLMI7 RL - implemented Expected SARSA

--- MLMI7-Reinforcement_Learning/Practical/code/expected_sarsa.py
+++ MLMI7-Reinforcement_Learning/Practical/code/expected_sarsa.py
@@ -1,3 +1,4 @@
+from re import A
 import sys
 from tqdm import tqdm
 import numpy as np
@@ -9,10 +10,15 @@ from model import Model, Actions
 
 
 
-def q_learning(model: Model, max_epochs: int = 100, maxit: int = 50, alpha: float = 0.2, epsilon: float = 0.1):
+def expected_sarsa(model: Model, max_epochs: int = 1000, maxit: int = 100, alpha: float = 0.2, epsilon: float = 0.1):
+    '''
+        References for some code:
+        https://medium.com/analytics-vidhya/q-learning-expected-sarsa-and-comparison-of-td-learning-algorithms-e4612064de97
+    '''
     V = np.zeros((model.num_states,))
     pi = np.zeros((model.num_states,))
     Q = np.zeros((model.num_states, len(Actions)))
+
 
     def choose_eps_greedily(s):
         rand_n = np.random.rand()
@@ -23,6 +29,29 @@ def q_learning(model: Model, max_epochs: int = 100, maxit: int = 50, alpha: floa
         
         idx = np.argmax(Q[s])
         return Actions(idx)
+
+    
+    def greedy_action_selection(s):
+        max_q = max(Q[s])
+        # there might be cases where there are multiple actions with the same high q_value.
+        # Choose randomly then
+        count_max_q = np.count_nonzero(Q[s] == max_q)
+        if count_max_q > 1:
+            # get all the actions with the maxQ
+            best_action_indexes = [i for i in range(len(Actions)) if Q[s][i] == max_q]
+            action_index = np.random.choice(best_action_indexes)
+        else:
+            action_index = np.where(Q[s] == max_q)[0]
+            
+        return Actions(action_index)
+
+
+    def action_probs(s):
+        next_state_probs = [epsilon/len(Actions)] * len(Actions)
+        best_action = greedy_action_selection(s)
+        next_state_probs[best_action] += (1.0 - epsilon)
+        return next_state_probs
+
 
     for _ in tqdm(range(max_epochs)):
         # init state
@@ -36,10 +65,11 @@ def q_learning(model: Model, max_epochs: int = 100, maxit: int = 50, alpha: floa
             new_s = np.random.choice(list(acts_probs_dict.keys()), p=list(acts_probs_dict.values()))
             # calculate reward
             r = model.reward(s, a)
-            # approximating the optimal action-value function
-            q = np.max(Q[new_s])
+            # calculate expected value of the action-value function
+            next_state_probs = action_probs(new_s)
+            expected_q = sum([a*b for a, b in zip(next_state_probs, Q[new_s])])
             # update Q using SARSA equation
-            Q[s][a] = Q[s][a] + alpha*(r + model.gamma*q - Q[s][a])
+            Q[s][a] = Q[s][a] + alpha*(r + model.gamma*expected_q - Q[s][a])
             # updating state
             s = new_s
             # checking if the new state is terminal
@@ -73,6 +103,6 @@ if __name__ == "__main__":
     else:
         model = Model(small_world)
 
-    V, pi = q_learning(model)
+    V, pi = expected_sarsa(model)
     plot_vp(model, V, pi)
     plt.show()
