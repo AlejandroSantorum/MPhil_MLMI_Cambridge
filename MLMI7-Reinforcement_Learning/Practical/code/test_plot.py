8dc050021fd4a3aec1f5f166c0afc0097d2c2c79
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-02-18 22:25:28 +0000

    MLMI7 RL - add some experiments and imgs for question d

--- MLMI7-Reinforcement_Learning/Practical/code/test_plot.py
+++ MLMI7-Reinforcement_Learning/Practical/code/test_plot.py
@@ -8,29 +8,29 @@ from q_learning import q_learning
 from expected_sarsa import expected_sarsa
 from model import Model
 
-from world_config import cliff_world, small_world, grid_world
+from world_config import cliff_world
+
+from scipy.ndimage.filters import uniform_filter1d
 
 
 if __name__ == '__main__':
     model = Model(cliff_world)
 
+    V_sarsa, pi_sarsa, cumR_sarsa, _ = sarsa(model, n_episodes=10000, maxit=100, alpha=0.3, decay_eps=True)
+    V_ql, pi_ql, cumR_ql, _ = q_learning(model, n_episodes=10000, maxit=100, alpha=0.3, decay_eps=True)
 
-    # V_pi, pi_pi = policy_iteration(model)
-    # V_vi, pi_vi = value_iteration(model)
-    V_sarsa, pi_sarsa, cumR_sarsa = sarsa(model)
-    V_ql, pi_ql, cumR_ql = q_learning(model)
-    #V_es, pi_es, cumR_es = expected_sarsa(model)
-
-    from scipy.ndimage.filters import uniform_filter1d
     y_sarsa = uniform_filter1d(cumR_sarsa, size=200)
     y_ql = uniform_filter1d(cumR_ql, size=200)
 
-    plt.style.use('seaborn')
+    #plt.style.use('seaborn')
 
-    plt.plot(range(len(y_sarsa)), y_sarsa, label='sarsa')
-    plt.plot(range(len(y_ql)), y_ql, label='ql')
-    min_val = min(np.min(y_sarsa), np.min(y_ql))
+    plt.plot(range(len(y_sarsa)), y_sarsa, label='SARSA')
+    plt.plot(range(len(y_ql)), y_ql, label='Q Learning')
+    min_val = max(np.min(y_sarsa), np.min(y_ql))
     plt.ylim(min_val, 0)
+    plt.xlabel("Number of episode")
+    plt.ylabel("Cumulated reward")
+    plt.title("Rewards obtained in each episode")
     plt.legend()
     plt.show()
 
