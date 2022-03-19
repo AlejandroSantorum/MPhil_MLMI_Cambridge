fb604bc1240b2362d4d84ba284f12aa32e7129e2
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-03-19 13:04:51 +0000

    MLMI8 NMT - exercise 1 done

--- MLMI8-Neural_Machine_Translation_&_Dialogue_Systems/practicals/GDBNMT/code/GDBNMT-Practical/wtoc.sh
+++ MLMI8-Neural_Machine_Translation_&_Dialogue_Systems/practicals/GDBNMT/code/GDBNMT-Practical/wtoc.sh
@@ -5,5 +5,18 @@ MAPPING_FILE=$GDBNMTBDIR/fsts/w+l.map.de
 python3 ./pyscripts/wtoc.py
 # compile FST
 fstcompile --isymbols=$MAPPING_FILE --osymbols=$MAPPING_FILE --keep_isymbols --keep_osymbols  fsts/wtoc.txt fsts/wtoc.fst
-# compose FST with acceptors
-fstcompose $GDBNMTBDIR/fsts/wmt18.sgnmt.wmt18ensemble.1/1.fst fsts/wtoc.fst tmp.fst
+
+if [ "$1" == "-ex" ]; then
+    TEMP_FST_FILE=fsts/tmp_ex1_1.fst
+
+    # compose FST with acceptors
+    fstcompose $GDBNMTBDIR/fsts/wmt18.sgnmt.wmt18ensemble.1/1.fst fsts/wtoc.fst $TEMP_FST_FILE
+
+    echo "Input baseline translation:"
+    $GDBNMTBDIR/printstrings.py --fst $TEMP_FST_FILE --syms $MAPPING_FILE --n 2
+
+    echo "Some sentences that contains possible class mappings:"
+    $GDBNMTBDIR/printstrings.py --fst $TEMP_FST_FILE --syms $MAPPING_FILE --n 2 --project_output
+
+    rm -f $TEMP_FST_FILE
+fi
